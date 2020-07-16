92b50669472a96661fe4e217fda251746f6fd96b
Brian Espinosa <b@bje.co>
2020-07-15 17:42:37 -0700

    Confirm CSS Modules will work with loaders in Cosmos 🙌🏽

--- ui/hello/src/HelloUI.js
+++ ui/hello/src/HelloUI.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from './HelloUI.module.css';
 
 export function HelloUI({ greeting, name }) {
   return (
-    <h1>
+    <h1 className={styles._}>
       {greeting}, {name}!
     </h1>
   );
