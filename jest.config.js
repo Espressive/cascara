bd1997d86bf9f758eb0469700a03d35f06ed2df9
Brian Espinosa <brianespinosa@users.noreply.github.com>
2020-07-28 08:03:26 -0700

    fix docz (#15)
    * Fix package.json to build component and not remove prop-types
    
    * Fix component name in README.mx for button
    
    * Get button working in docs with fixed rollbar config
    
    * Remove extra deploy step from main since it is not needed
    
    * Cleanup the build process on doc creation
    
    * Add the .docz folder to ignore so we do not try to run tests there either
    
    * Make the build step actually build so we know if there are docz problems
    
    * Try a new build config…
    
    * fix node tag
    
    * See if we can remove the executor in next flows. And also if the install is faster now from cache
    
    * Set the executor since it is required
    
    * Put executor tags back. Looks like Circle CI is spinning these up in different containers each time.

--- jest.config.js
+++ jest.config.js
@@ -150,9 +150,7 @@ module.exports = {
   testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  // testPathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  testPathIgnorePatterns: ['.docz', '/node_modules/'],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
