68e248bf59a4f94927cb69ee14a2cc226e827121
Brian Espinosa <b@bje.co>
2020-07-30 21:21:25 -0700

    Add some customizations to Docz and also netlify.toml

--- doczrc.js
+++ doczrc.js
@@ -3,4 +3,17 @@ export default {
   dest: '/build/docs',
   // menu: ['Introduction', 'Variables', 'UI', 'Layouts'],
   port: 4000,
+  themeConfig: {
+    showDarkModeSwitch: false,
+    showMarkdownEditButton: false,
+    colors: {
+      modes: {
+        dark: {
+          playground: {
+            bg: '#f9f9f9',
+          },
+        },
+      },
+    },
+  },
 };
