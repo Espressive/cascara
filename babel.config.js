162b035fc78be2f17db4299daa317bb1d093405b
Brian Espinosa <b@bje.co>
2020-07-31 13:59:33 -0700

    Update our babel config for all of our Rollup needs and separate Jest in an env

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,25 @@
-// module.exports = {
-//   presets: [
-//     [
-//       'espressive',
-//       {
-//         targets: {
-//           node: 'current',
-//         },
-//       },
-//     ],
-//   ],
-// };
-
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
-  plugins: [
+  presets: [
     [
-      '@babel/plugin-proposal-class-properties',
+      '@babel/preset-env',
       {
+        modules: false,
         loose: true,
       },
     ],
+    '@babel/preset-react',
   ],
+  plugins: [
+    '@babel/plugin-proposal-optional-chaining',
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+  ],
+  env: {
+    // Make sure we use these plugins when running Jest
+    test: {
+      plugins: [
+        '@babel/plugin-transform-modules-commonjs',
+        '@babel/plugin-transform-runtime',
+      ],
+    },
+  },
 };
