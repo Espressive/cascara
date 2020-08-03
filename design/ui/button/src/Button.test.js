2782e8df792045df1d3f84c2022ed6582db84a6c
Brian Espinosa <b@bje.co>
2020-08-03 15:50:26 -0700

    Update Jest config, add sectioned tests for button

--- design/ui/button/src/Button.test.js
+++ design/ui/button/src/Button.test.js
@@ -1,14 +1,113 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import { Router, Link } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+
 import { Button } from './Button';
 
-test('Renders with defaults', () => {
-  const snap = render(<Button />).container;
+describe('Button', () => {
+  describe('Default', () => {
+    const testId = 'default';
 
-  expect(snap).toMatchSnapshot();
+    // We need a place to store the view for snapshot testing. This is not required when we are using `screen` directly from RTL.
+    let view;
 
-  // This should always be our first test rendering the bare component.
-  // We might need to find a way to make this test not fail for a prop-type
-  // error if there are missing required props. Eventually we want tests
-  // to fail if there are any console errors.
+    beforeEach(() => {
+      // Set the render container to our `view` so it is in scope for the snapshot test
+      view = render(<Button data-testid={testId} />).container;
+    });
+
+    test('renders without any props', () => {
+      expect(view).toMatchSnapshot();
+    });
+
+    test('renders a <button> by default', () => {
+      const button = screen.getByTestId(testId);
+      // Make sure the actual DOM element type is BUTTON
+      expect(button.tagName).toMatch('BUTTON');
+      // Check that we also use the correct type for accessibility
+      expect(button).toHaveAttribute('type', 'button');
+      // Check that we do not use the redundant "role" attribute when the tag is BUTTON
+      expect(button).not.toHaveAttribute('role');
+    });
+  });
+
+  describe('as', () => {
+    describe('<div />', () => {
+      test('has correct a11y attributes', () => {
+        const props = {
+          as: 'div',
+          'data-testid': 'as-div',
+        };
+        render(<Button {...props} />);
+
+        const button = screen.getByTestId(props['data-testid']);
+
+        // Should not have a type anymore, but SHOULD have a role
+        expect(button).not.toHaveAttribute('type');
+        expect(button).toHaveAttribute('role', 'button');
+      });
+    });
+
+    describe('<a />', () => {
+      test('has correct a11y attributes', () => {
+        const props = {
+          as: 'a',
+          'data-testid': 'as-anchor',
+          href: 'https://google.com',
+        };
+
+        render(<Button {...props} />);
+
+        const button = screen.getByTestId(props['data-testid']);
+
+        // Should not have either role or type if we render an anchor
+        expect(button).not.toHaveAttribute('type');
+        expect(button).not.toHaveAttribute('role');
+        expect(button).toHaveAttribute('href', props.href);
+        expect(button).not.toHaveAttribute('rel');
+      });
+
+      test('has secure rel attribute for external links', () => {
+        const props = {
+          as: 'a',
+          'data-testid': 'as-anchor-target',
+          href: 'https://google.com',
+          target: '_blank',
+        };
+
+        render(<Button {...props} />);
+
+        const button = screen.getByTestId(props['data-testid']);
+
+        // Make sure we render the target
+        expect(button).toHaveAttribute('target', props.target);
+        // Make sure we have a secure rel attribute for a link with a target
+        expect(button).toHaveAttribute('rel', 'noopener noreferrer');
+      });
+    });
+
+    describe('<Component />', () => {
+      test('renders a React component', () => {
+        const props = {
+          as: Link,
+          to: '/users',
+          'data-testid': 'as-component',
+        };
+
+        const history = createMemoryHistory();
+
+        render(
+          <Router history={history}>
+            <Button {...props} />
+          </Router>
+        );
+
+        const button = screen.getByTestId(props['data-testid']);
+
+        // Make sure this renders a link (from the Link component)
+        expect(button.tagName).toMatch('A');
+      });
+    });
+  });
 });
