02606bd3ce74e4cbe353f680c349ef937190027c
Manu Ramirez <manu.ramirez@espressive.com>
2020-09-21 14:25:06 -0500

    fix actionBar in footer

--- packages/cascara/src/ui/Table/TableFooter.js
+++ packages/cascara/src/ui/Table/TableFooter.js
@@ -32,48 +32,53 @@ const TableFooter = () => {
     headerCells.push(<th key={'action-bar-slot'} />);
   }
 
+  const actionBar =
+    selection.length && bulkActions.length ? (
+      <tr>
+        <th
+          colSpan={headerCells.length}
+          scope={'colgroup'}
+          style={{
+            background: '#5FB59D',
+            display: 'flex',
+            gridColumn: `1/${headerCells.length + 1}`,
+            justifyContent: 'space-between',
+          }}
+        >
+          <ActionBar
+            actions={bulkActions.map((action) => {
+              let Component;
+
+              switch (action.module.type) {
+                case 'download':
+                  Component = DownloadButton;
+                  break;
+
+                default:
+                  Component = Button;
+              }
+
+              return (
+                <Component
+                  {...action}
+                  content={action.label}
+                  key={action.label}
+                  onClick={() => handleOnAction(action)}
+                />
+              );
+            })}
+            title={`${selection.length} selected`}
+          />
+        </th>
+      </tr>
+    ) : (
+      ''
+    );
+
   return (
     <tfoot>
       <tr>{headerCells}</tr>
-      {selection.length && bulkActions.length && (
-        <tr>
-          <th
-            colSpan={headerCells.length}
-            scope={'colgroup'}
-            style={{
-              background: '#5FB59D',
-              display: 'flex',
-              gridColumn: `1/${headerCells.length + 1}`,
-              justifyContent: 'space-between',
-            }}
-          >
-            <ActionBar
-              actions={bulkActions.map((action) => {
-                let Component;
-
-                switch (action.module.type) {
-                  case 'download':
-                    Component = DownloadButton;
-                    break;
-
-                  default:
-                    Component = Button;
-                }
-
-                return (
-                  <Component
-                    {...action}
-                    content={action.label}
-                    key={action.label}
-                    onClick={() => handleOnAction(action)}
-                  />
-                );
-              })}
-              title={`${selection.length} selected`}
-            />
-          </th>
-        </tr>
-      )}
+      {actionBar}
     </tfoot>
   );
 };
