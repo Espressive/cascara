f76ebfa6ed4c8b1b5cc6808de5ccebd4fc158911
Brian Espinosa <b@bje.co>
2020-10-03 13:17:49 -0700

    FDS-2 Scaffolding for DataCheckbox

---
+++ packages/cascara/src/modules/DataCheckbox/DataCheckbox.js
@@ -0,0 +1,34 @@
+import React, { useContext } from 'react';
+import { ModuleContext } from '../context';
+
+const DataCheckbox = ({
+  isEditable = true,
+  value,
+  label = 'DataCheckbox',
+  ...rest
+}) => {
+  const { isEditing, formMethods } = useContext(ModuleContext);
+
+  const renderEditing = (
+    <input
+      {...rest}
+      defaultValue={value}
+      name={label}
+      ref={formMethods?.register}
+    />
+  );
+
+  const renderDisplay = (
+    <>
+      {label && <div className='ui large label'>{label}</div>}
+      <div style={{ display: 'inline-block', padding: '.5em 1em' }}>
+        {value}
+      </div>
+    </>
+  );
+
+  // Do not render an editable input if the module is not editable
+  return isEditing && isEditable ? renderEditing : renderDisplay;
+};
+
+export default DataCheckbox;
