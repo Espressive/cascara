6a8b649be7a2161936aebdb8769d3493e7725d4d
Brian Espinosa <b@bje.co>
2021-02-17 13:26:59 -0800

    Define danger

--- .danger/modified.ts
+++ .danger/modified.ts
@@ -1,6 +1,8 @@
-import { danger, warn } from 'danger';
+import { danger } from 'danger';
 
 // Alert that snapshot files have been changed
 export const snapshots = () => {
-  danger.git.modified_files.map((file) => console.log(file));
+  if (danger.git.modified_files.length > 1) {
+    const files = danger.git.modified_files.map((file) => console.log(file));
+  }
 };
