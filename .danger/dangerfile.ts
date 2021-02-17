073ff0c42900b991a47c5eab031e96bd3dfa98d2
Brian Espinosa <b@bje.co>
2021-02-17 15:51:22 -0800

    dependency rules

--- .danger/dangerfile.ts
+++ .danger/dangerfile.ts
@@ -4,6 +4,10 @@ const modifiedFiles = danger.git.modified_files;
 const newFiles = danger.git.created_files;
 const changedFiles = [...modifiedFiles, ...newFiles];
 
+const changedPackages = changedFiles.filter((file) =>
+  file.includes('package.json')
+);
+
 // No PR is too small to include a description of why you made a change
 if (danger.github.pr.body.length < 10) {
   warn('Please include a description of your PR changes.');
@@ -16,8 +20,11 @@ if (danger.github.pr.assignee === null) {
   );
 }
 
-if (modifiedFiles.length > 1) {
-  for (let file of modifiedFiles) {
-    message(file);
+// Check if a package.json file has been modified anywhere
+if (changedPackages) {
+  for (let file of changedPackages) {
+    warn(
+      `**${file}**: Please provide a reason that we are making changes to package dependencies`
+    );
   }
 }
