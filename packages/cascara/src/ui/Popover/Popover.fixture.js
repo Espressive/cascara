15a54a5f26821cf267591042a4e98df927779945
Brian Espinosa <b@bje.co>
2021-02-17 14:00:19 -0800

    Clean up fixtures for ActionsMenu

--- packages/cascara/src/ui/Popover/Popover.fixture.js
+++ packages/cascara/src/ui/Popover/Popover.fixture.js
@@ -14,12 +14,23 @@ const menuItems = [
   },
 ];
 
-const Fixture = () => (
+const PopoverFixture = (
   <main className='ui container'>
     <h1>Popover</h1>
 
     <Popover trigger={<Button content='Greeting' />}>Hello!</Popover>
+  </main>
+);
 
+const ActionsMenuFixture = (
+  <main className='ui container'>
+    <h1>ActionsMenu</h1>
+    <p>
+      This uses Popover from Reakit under the hood. This new version of Reakit
+      allows us to use modifiers on popper.js. For now we will be using this
+      inside Table Actions to stack any of our `actions.length &gt; 1` inside of
+      this menu.
+    </p>
     <ActionsMenu
       actions={menuItems}
       trigger={<Button className='icon' content='⋯' />}
@@ -27,4 +38,6 @@ const Fixture = () => (
   </main>
 );
 
-export default Fixture;
+/* eslint-disable sort-keys */
+export default { Popover: PopoverFixture, ActionsMenu: ActionsMenuFixture };
+/* eslint-enable sort-keys */
