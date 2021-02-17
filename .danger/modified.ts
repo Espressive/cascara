6e02986d50227ed45095388be318fbffa1e49611
Brian Espinosa <b@bje.co>
2021-02-17 14:35:44 -0800

    fixed name in modified

--- .danger/modified.ts
+++ .danger/modified.ts
@@ -1,6 +1,6 @@
 import { danger, message } from 'danger';
 
-export const assignees = () => {
+export const modified = () => {
   // No PR is too small to include a description of why you made a change
   if (danger.git.modified_files.length > 1) {
     message('You have modified more than one file');
