9cc0c68b2ae84f27ad4606cc8a4d73df56dcf427
Brian Espinosa <b@bje.co>
2021-03-02 06:15:52 -0800

    Rename eslint config for scoped use

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 // We should almost definitely not be adding rules here. Instead, we should be modifying rules in our eslint-config-espressive package
 
 module.exports = {
-  extends: '@espressive/eslint-config-espressive',
+  extends: '@espressive',
   overrides: [
     {
       files: ['**/next-*/**/*', 'docs/**/*'],
