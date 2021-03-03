70f9a9f8d926358cb49b3b5fdc4774caae9b875e
Brian Espinosa <b@bje.co>
2021-03-03 12:39:23 -0800

    chore(FDS-146) Resolve eslint `prefer-` prefixed errors

--- packages/cascara/src/ui/Form/poc/FormPublicAPI.fixture.js
+++ packages/cascara/src/ui/Form/poc/FormPublicAPI.fixture.js
@@ -92,6 +92,49 @@ const dataConfig = {
   ],
 };
 
+const getValue = (data, config) => {
+  return { ...config, defaultValue: data[config.attribute] };
+};
+
+const prepareRowData = (data, rowConfig) =>
+  rowConfig.map((field) => getValue(data, field));
+
+// We will want to make this more robust to test multi-dimensional arrays and
+// then include it in the prepareFormData switch
+const isRow = (value) => {
+  switch (typeof value[0]) {
+    case 'undefined':
+      return false;
+    case 'object':
+      return true;
+    default:
+      throw new Error('Bad data but we do not know why right now');
+  }
+};
+
+const prepareFormData = (data, config) => {
+  const fields = config.display.map((field) => {
+    switch (typeof field[0]) {
+      // This case is actually for an object at the root of the array
+      case 'undefined':
+        return getValue(data, field);
+      // This case is technically telling us we have an object at the first index
+      // which means we have a multi-dimensional array.
+      case 'object':
+        return prepareRowData(data, field);
+      default:
+        throw new Error('Only objects or arrays are supported in data.display');
+    }
+  });
+
+  const { actions } = dataConfig;
+
+  return {
+    actions,
+    fields,
+  };
+};
+
 const jsonStyle = {
   display: 'inline-block',
   verticalAlign: 'top',
