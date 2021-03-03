bbe31e730cb8bd63677fcfa9b892011d2acef876
Brian Espinosa <b@bje.co>
2021-03-03 13:41:11 -0800

    chore(FDS-151): Resolve some eslint react rules (button type, display name, func comp, jsx bool)
    Resolves FDS-151
    
    ## Fixes rules
    
    ```
    'react/button-has-type': 0,
    'react/display-name': 0,
    'react/function-component-definition': 0,
    'react/jsx-boolean-value': 0,
    ```

--- app-tests/_templates/next-template/src/pages/index.js
+++ app-tests/_templates/next-template/src/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import pkg from '../../package';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
+const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -17,4 +17,6 @@ export default function Home() {
       </main>
     </div>
   );
-}
+};
+
+export default Home;
