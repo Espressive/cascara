d6d0665620dadfaefe0a39cf3a41332aee177866
Brian Espinosa <b@bje.co>
2021-04-27 13:52:07 -0700

    Simplify config

--- .cosmos/v4.webpack.override.js
+++ .cosmos/v4.webpack.override.js
@@ -1,31 +1,9 @@
 // NOTE: This file is for the v4 version of react-scripts
+
 const path = require('path');
-const workspaces = require('../package.json').workspaces;
 const cosmosConfig = require('./config.json');
 
 module.exports = (webpackConfig, env) => {
-  // Creates an array of workspace packages for us to look in.
-  // Function accepts a workspace definition
-  const allSrc = (packages) => {
-    // Start with an empty array
-    let allPackages = [];
-
-    // Look in all package definition folders first
-    packages.forEach((pkg) => {
-      const repoPacks = glob.sync(pkg);
-
-      // Look at all packages in the folder
-      repoPacks.forEach((repoPkg) => {
-        // Use our full path resolution function to define the src folder for each
-        const pkgSrc = resolveApp(repoPkg + '/src');
-        // Add the folder to our array of src to process with Babel
-        allPackages.push(pkgSrc);
-      });
-    });
-
-    return allPackages;
-  };
-
   // Set the html template file to be the one defined in the cosmos staticPath
   webpackConfig.plugins[0].options.template = path.resolve(
     cosmosConfig.staticPath,
@@ -38,10 +16,10 @@ module.exports = (webpackConfig, env) => {
   webpackConfig.module.rules[1].oneOf[3].options.configFile = true;
 
   // Rules index 4-7 are all for styles. The last two use resolve-url-loader which
-  // needs to have the root defined relative to the workspace assets
+  // needs to have the root defined relative to the workspace assets. Setting 'root'
+  // to empty allows sass to resolve the file assets as it normally would for development
   webpackConfig.module.rules[1].oneOf[7].use[3].options.root = '';
   webpackConfig.module.rules[1].oneOf[6].use[3].options.root = '';
-  console.log(webpackConfig.module.rules[1].oneOf[7].use[3]);
 
   return { ...webpackConfig };
 };
