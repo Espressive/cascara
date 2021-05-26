f61cdd0e399efe128aeb142682e19ea5e8705615
Manu Ramirez <manu.ramirez@espressive.com>
2021-05-26 13:54:30 -0500

    chore(FDS-222): export icons as commonjs + fix for docs paths

--- packages/icons/src/buildCollection.js
+++ packages/icons/src/buildCollection.js
@@ -102,21 +102,29 @@ tools
 
       const iconFile = `var data = {body: '${svg.getBody()}', height: ${
         svg.height
-      }, width: ${svg.width}};\nexport default data`;
+      }, width: ${svg.width}};\nmodule.exports = data`;
 
       fs.writeFileSync(`${name}.js`, iconFile);
     });
 
-    const indexFile = collectionNames
+    const indexFileImportSection = collectionNames
+      .sort()
       .map(
         (iconName) =>
-          `export { default as ${snakeToCamel(
-            iconName
-          )}Icon } from './${iconName}';`
+          `const ${snakeToCamel(iconName)}Icon = require ('./${iconName}');`
       )
       .join('\n');
 
-    fs.writeFileSync('index.js', indexFile);
+    const indexFileExportSection = `module.exports = {
+        ${collectionNames
+          .sort()
+          .map((iconName) => ` ${snakeToCamel(iconName)}Icon,`)
+          .join('\n')}`;
+
+    fs.writeFileSync(
+      'index.js',
+      `${indexFileImportSection}\n${indexFileExportSection}};`
+    );
   })
   .catch((err) => {
     console.error(err);
