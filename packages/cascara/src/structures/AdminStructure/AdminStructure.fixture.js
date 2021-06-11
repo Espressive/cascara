78a31035f28660e0c969b88e45b1133e575eb236
Brian Espinosa <b@bje.co>
2021-06-11 14:22:22 -0700

    Fix missing scroll support for main

--- packages/cascara/src/structures/AdminStructure/AdminStructure.fixture.js
+++ packages/cascara/src/structures/AdminStructure/AdminStructure.fixture.js
@@ -179,7 +179,124 @@ export default {
   ),
   'no-drawer': (
     <AdminStructure header={exampleHeader} nav={exampleNav}>
-      <AdminStructure.Main header='Main' />
+      <AdminStructure.Main header='Main'>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus
+          voluptatum eaque vel at laudantium, eligendi nisi exercitationem
+          commodi eum ipsum qui veritatis saepe suscipit impedit, quisquam nemo
+          aliquam pariatur aspernatur eos amet iusto ex optio perferendis a. Cum
+          iste cupiditate non blanditiis eius delectus placeat ut voluptates
+          nemo praesentium. Facere sequi ratione in non, velit voluptates
+          accusantium cumque iste perspiciatis, alias quidem fuga nobis
+          reprehenderit porro! Eaque nam rem, non, soluta at delectus molestiae
+          esse consequuntur eveniet, voluptate culpa! Laboriosam deleniti,
+          maxime nisi, quidem adipisci quis sapiente culpa eligendi tenetur
+          autem facere est omnis. Natus aliquid adipisci quas totam id nemo
+          beatae velit, magnam, corporis facilis blanditiis, officiis
+          perspiciatis! Ipsum minus saepe temporibus dignissimos consequuntur
+          corrupti velit veniam rerum eligendi ullam aspernatur nesciunt non aut
+          nihil, facere laborum. Aliquid molestias laboriosam perspiciatis
+          mollitia quisquam sed, ut odio voluptates enim, cupiditate minus
+          itaque quia quam tempore officiis iusto hic ipsam! Dignissimos dicta
+          quas illum, sequi ipsa dolores dolorum quod tempora qui quaerat et
+          veritatis labore odio, amet nisi. Porro, et nostrum expedita possimus
+          voluptas adipisci perferendis. Nemo similique distinctio quos debitis
+          assumenda? Distinctio, similique praesentium sequi quod eveniet non
+          quas illum, hic aliquid magni labore fuga incidunt aut pariatur quasi
+          sunt cumque modi earum qui dolorum omnis, quam quae! Corrupti illo
+          quisquam voluptas doloribus quae similique quam ratione ipsa eligendi
+          hic velit omnis odit voluptatibus, nostrum pariatur qui ipsam ea
+          incidunt possimus inventore nisi id? Officiis pariatur ullam explicabo
+          iste sequi consequatur, sit voluptate numquam fugit, perferendis
+          minus, quis expedita. Sit modi perspiciatis, recusandae corporis ut
+          expedita vel voluptatem sint necessitatibus odio unde, dolores soluta
+          fugit esse sequi, voluptas qui facilis consequuntur. Quo ab deleniti
+          maiores. Facere accusantium vitae nesciunt totam vero nulla deserunt
+          iste sed possimus, nostrum, necessitatibus molestias ratione. Harum
+          incidunt reprehenderit magnam laudantium, mollitia iste labore,
+          doloremque nobis laborum beatae praesentium repudiandae aliquam
+          impedit in qui obcaecati alias necessitatibus fugit debitis corporis
+          sit nesciunt. Quos reprehenderit dolores deleniti, nesciunt commodi
+          ipsum doloremque fugiat mollitia aut distinctio similique harum
+          molestiae obcaecati sapiente maxime consequatur adipisci laborum
+          totam! Facilis illum modi totam, obcaecati dolores unde nam provident
+          architecto animi cupiditate error dolor, autem reprehenderit ad
+          tempora officiis in laudantium consequuntur. Non, commodi! Itaque
+          beatae enim distinctio numquam dolore hic repellat labore pariatur
+          accusantium praesentium? Similique cumque cum aperiam voluptate
+          quaerat consequatur exercitationem cupiditate saepe doloribus sint
+          magni consequuntur facilis eaque doloremque eveniet, provident
+          laboriosam accusamus repellat officiis numquam earum vero incidunt.
+          Quas nesciunt quam ad ducimus ipsa, perspiciatis sint dignissimos
+          cupiditate ratione dicta repellendus aliquid eius iure vel saepe
+          totam, mollitia, pariatur placeat odit. Placeat, veritatis molestias?
+          Animi inventore facilis provident magni pariatur, consequuntur at
+          impedit placeat porro quo suscipit quae! Veritatis velit sapiente quod
+          quaerat aliquid expedita eaque quam repudiandae corrupti voluptas
+          molestias quidem optio enim laboriosam commodi, illum mollitia? Sequi
+          excepturi soluta, in cumque omnis culpa eaque, officiis tempora
+          voluptatibus optio ullam, temporibus nesciunt obcaecati sed quas
+          consequatur fugiat quaerat nisi. Maxime dolore veniam non quisquam,
+          dicta minima labore eligendi ipsum accusantium mollitia porro commodi
+          ipsa qui doloremque natus quam, enim necessitatibus dolores ducimus
+          ullam? Consectetur vel eveniet porro cum fugit animi quisquam
+          nesciunt. Expedita fugiat voluptate, sed eligendi, porro quas corrupti
+          ducimus asperiores, sint in reprehenderit temporibus iusto eius.
+          Praesentium illo error hic cumque debitis explicabo deleniti labore
+          repellat, ducimus eaque sint, ipsam iure veniam voluptates aut vero
+          quae, quas at? Eius animi libero esse error unde ea numquam labore
+          nulla deserunt sequi rerum quis, tempore debitis adipisci similique
+          natus ipsam illum, maxime, quam voluptatibus quibusdam placeat ducimus
+          repellat! Totam eligendi itaque saepe perferendis blanditiis nihil?
+          Obcaecati corporis veritatis est amet nihil maxime quis repellat
+          recusandae, quo a excepturi veniam optio beatae fugit, eligendi
+          dolores modi perferendis odio exercitationem. Deleniti nemo sapiente
+          ipsum accusamus voluptates fuga optio nam inventore facere expedita,
+          recusandae dolorum, nobis earum explicabo facilis eveniet cupiditate
+          ducimus quae! Fugiat ex mollitia sint eveniet dolorem voluptas labore
+          molestiae architecto accusamus illo natus, ab, deleniti distinctio
+          modi expedita fuga? Nostrum suscipit amet alias rem, minima eos,
+          exercitationem ad dolores at itaque neque laborum harum iste sapiente
+          doloribus, cumque vero asperiores sunt quo aperiam maiores mollitia!
+          Adipisci dicta soluta autem labore maxime, est praesentium ducimus
+          necessitatibus magni eius possimus porro non ipsa deserunt modi ullam
+          consequatur laboriosam molestias optio magnam. Delectus sit deleniti,
+          placeat neque beatae numquam quidem saepe aspernatur itaque labore
+          porro ex voluptas et voluptate nemo, eum fugit error voluptatibus!
+          Laudantium quibusdam cumque ea et voluptates, magni similique debitis
+          recusandae sequi natus unde architecto sint reprehenderit, delectus
+          exercitationem, ex incidunt! Laborum ex maiores eum illo odit ullam
+          hic vel aliquid. Nostrum ea doloremque eligendi omnis nihil.
+          Perferendis reprehenderit dolor voluptatem corporis repellat doloribus
+          atque molestiae mollitia! Nemo necessitatibus porro sunt inventore nam
+          impedit fuga consequatur labore aliquam fugit sapiente hic, doloribus
+          at perspiciatis libero dicta. Architecto quod laudantium veniam
+          facilis quae vero, repellat quam laborum blanditiis odio esse porro
+          quis corporis, deserunt nam animi eveniet, nobis perferendis? Fugit
+          deleniti atque ex perferendis veritatis cupiditate rem ea distinctio
+          dolores officiis earum omnis delectus, temporibus aperiam quo labore
+          vel reiciendis pariatur harum illo animi? Illo non eveniet aspernatur
+          facilis, alias, enim suscipit numquam ullam deleniti nihil, fuga
+          ratione nobis id sequi recusandae repellendus molestiae!
+          Necessitatibus alias maxime nam. Officia, ab et voluptas possimus
+          consectetur facere quibusdam architecto, porro ipsa magni voluptatum
+          placeat rem praesentium minima ipsam nemo laudantium! Illo aliquid
+          odit quo reprehenderit molestiae minus culpa, voluptas numquam dolores
+          incidunt sunt soluta dolorum repudiandae eaque corporis fugit pariatur
+          aliquam labore tempore in, aspernatur laboriosam, similique unde?
+          Perspiciatis, animi! Ad quae neque in omnis vero nostrum consequuntur,
+          maxime, dolore nam fugiat, fuga ut repudiandae facilis facere
+          quibusdam blanditiis cumque totam ipsa pariatur? Libero, repudiandae
+          ducimus error ad delectus eligendi laboriosam voluptates unde, tempora
+          incidunt beatae qui totam iure nam earum molestias vitae expedita!
+          Esse iste officiis ducimus, ex quisquam modi reiciendis impedit iusto
+          accusantium tempora adipisci temporibus cumque, repudiandae laboriosam
+          laudantium facere quas praesentium fugiat enim molestiae. Laudantium
+          nam velit tempora dignissimos illum delectus at, ut laboriosam, iusto
+          nulla quo, excepturi culpa consequuntur rem inventore dolorem corrupti
+          harum!{' '}
+        </div>
+      </AdminStructure.Main>
     </AdminStructure>
   ),
   scrolling: <AdminStructure header={longContent} nav={longContent} />,
