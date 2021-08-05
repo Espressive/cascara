eeb3fe68f8588a50383e7563351711b370aba1fd
Brian Espinosa <b@bje.co>
2021-08-05 13:06:38 -0700

    Wow. We were not exporting ChatProvider. :/

--- packages/cascara/src/private.js
+++ packages/cascara/src/private.js
@@ -3,7 +3,7 @@ export * from './hooks';
 export * from './lib';
 
 // ChatProvider will eventually move to Public or be attached to Chat like Chat.Provider
-export { default as ChatProvider } from './ui/Chat';
+export { default as ChatProvider } from './ui/Chat/ChatProvider';
 
 // We may need to figure out a better way of defining these
 export { default as ActionEditTable } from './ui/Table/modules/ActionEdit';
