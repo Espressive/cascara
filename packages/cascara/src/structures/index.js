a2818dc056bf53cdac08767b7e07629d35729ce7
Brian Espinosa <b@bje.co>
2021-09-13 10:34:28 -0700

    Simplify exports

--- packages/cascara/src/structures/index.js
+++ packages/cascara/src/structures/index.js
@@ -1,5 +1,6 @@
 // HEADS UP!
-// Everything exported here is being exported from Cascara
+// Everything exported here is being exported from Cascara.
+// Let's keep these sorted alphabetically based on their source directory.
 
 export { default as Admin } from './Admin';
 export { default as AdminStructure } from './AdminStructure';
