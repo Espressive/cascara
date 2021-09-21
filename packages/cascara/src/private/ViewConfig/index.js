f3b5172e56b54c9d7b20a1c33c9a7e2bfc9a2453
Brian Espinosa <brianespinosa@users.noreply.github.com>
2021-09-21 12:32:45 -0700

    chore(FDS-349): ViewConfig improvements (#275)
    * chore(FDS-349): ViewConfig default icon to eye
    
    * chore(FDS-349): ViewConfig default icon to eye

--- packages/cascara/src/private/ViewConfig/index.js
+++ packages/cascara/src/private/ViewConfig/index.js
@@ -1,2 +1,3 @@
 export { default as useViewConfigState } from './useViewConfigState';
+export { VIEW_CONFIG_PROP_TYPES as viewConfigPropTypes } from './__globals';
 export { default } from './ViewConfig';
