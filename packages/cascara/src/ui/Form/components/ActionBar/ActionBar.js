ed679563c126a1f1d8a2aaab6bc6450eb0318d5a
rocio.baena <rocio.baena@espressive.com>
2021-12-01 00:10:37 -0600

    apply feedback

--- packages/cascara/src/ui/Form/components/ActionBar/ActionBar.js
+++ packages/cascara/src/ui/Form/components/ActionBar/ActionBar.js
@@ -8,21 +8,16 @@ import { propTypes as actionEditPT } from '../../modules/ActionEdit';
 import styles from '../../Form.module.scss';
 import { Boundaries } from '../../../../system-components';
 
-import classnames from 'classnames/bind';
-
 const propTypes = {
   actions: pt.arrayOf(
     pt.oneOfType([pt.shape(actionButtonPT), pt.shape(actionEditPT)])
   ),
-  className: pt.string,
 };
 
-const cx = classnames.bind(styles);
-
-const ActionBar = ({ actions, className }) => {
+const ActionBar = ({ actions }) => {
   return (
     <Boundaries>
-      <div className={cx(className, 'FormActionBar')}>{actions}</div>
+      <div className={styles.FormActionBar}>{actions}</div>
     </Boundaries>
   );
 };
