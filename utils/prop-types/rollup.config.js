58c43d6f8035fb3f5d8212e59efdbcd8b71ee0f6
rocio.baena <rocio.baena@espressive.com>
2021-12-17 12:58:27 -0600

    test rollup config

--- utils/prop-types/rollup.config.js
+++ utils/prop-types/rollup.config.js
@@ -1,20 +1,24 @@
-/**
- * This is custom because the root config breaks on this path.
- * Since we are not exporting any JSX nor SCSS, we don't need extra plugins.
- */
-const rollupConfig = {
-  external: ['prop-types'],
-  input: 'src/index.js',
-  output: [
-    {
-      file: 'dist/cjs/index.js',
-      format: 'cjs',
-    },
-    {
-      file: 'dist/es/index.js',
-      format: 'es',
-    },
-  ],
-};
+// /**
+//  * This is custom because the root config breaks on this path.
+//  * Since we are not exporting any JSX nor SCSS, we don't need extra plugins.
+//  */
+// const rollupConfig = {
+//   external: ['prop-types'],
+//   input: 'src/index.js',
+//   output: [
+//     {
+//       file: 'dist/cjs/index.js',
+//       format: 'cjs',
+//     },
+//     {
+//       file: 'dist/es/index.js',
+//       format: 'es',
+//     },
+//   ],
+// };
 
-export default rollupConfig;
+// export default rollupConfig;
+
+import getRollupConfig from '../../rollup.config';
+
+export default getRollupConfig({ pwd: __dirname });
