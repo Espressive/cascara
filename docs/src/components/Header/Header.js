af53548c25572215f4286916f8b8eecefd3f9994
Brian Espinosa <brianespinosa@users.noreply.github.com>
2021-12-20 15:24:15 -0800

    chore(FDS-400): Prepare docs for v1.0.0 release (#324)
    * Header links to version/branch
    
    * Remove framer-motion dep
    
    * Fix strange stuff in nav structure
    
    * Do not change title as coded in MDX

--- docs/src/components/Header/Header.js
+++ docs/src/components/Header/Header.js
@@ -15,8 +15,18 @@ const Header = ({ branch, cascaraVersion }) => {
       post={
         <div>
           <Flex>
-            <Tag content={cascaraVersion} />
-            {branch !== 'main' && <Tag content={branch} />}
+            {cascaraVersion && (
+              <a
+                href={`https://github.com/Espressive/cascara/packages/361656?version=${cascaraVersion}`}
+              >
+                <Tag content={cascaraVersion} />
+              </a>
+            )}
+            {branch && branch !== 'main' && (
+              <a href={`https://github.com/Espressive/cascara/tree/${branch}`}>
+                <Tag content={branch} />
+              </a>
+            )}
           </Flex>
         </div>
       }
--- docs/src/components/Nav/NavLinkNext.js
+++ docs/src/components/Nav/NavLinkNext.js
@@ -7,17 +7,15 @@ const propTypes = {
 };
 
 // This component is needed as a compatibility layer inside of AdminStructure.Nav due to the odd way that
-// Link works inside of `next/link` where it does not render an element itself. This component allows us to 
+// Link works inside of `next/link` where it does not render an element itself. This component allows us to
 // maintain a structure that is consistent with other link components, and direct props into the expected location.
 const NavLinkNext = ({ children, isActive, ...rest }) => {
   const classList = [rest.className, isActive ? 'active' : undefined];
 
   return (
-    <div className={classList.join(' ')}>
-      <Link {...rest}>
-        <a>{children}</a>
-      </Link>
-    </div>
+    <Link {...rest}>
+      <a className={classList.join(' ')}>{children}</a>
+    </Link>
   );
 };
 
