c7399f5c78edb0c891cfb4687da78b611f3f8527
rocio.baena <rocio.baena@espressive.com>
2021-12-31 15:43:34 -0600

    chore(FDS-38):[Tests]: Improve unit test for input text

--- packages/cascara/src/modules/DataText/DataText.test.js
+++ packages/cascara/src/modules/DataText/DataText.test.js
@@ -1,106 +1,198 @@
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import fixtures from '../helpers/ModuleTestHelper.fixture';
+import cosmosFixtures, { displayProps, editingProps } from './DataText.fixture';
+import testFixtures from '../helpers/ModuleTestHelper.fixture';
 
-const { textTest } = fixtures;
+// We cannot destructure during import because the default export in Cosmos
+// multi-fixture files is an object so we need to import the fixtures first,
+// then destructure them separately.
+const { display, editing, editingNoLabel } = cosmosFixtures;
 
-describe('dataText', () => {
-  /**
-   * This test validates the data types going in are also going out.
-   * i.e. without any type casting that corrupts the data.
-   *
-   * The test focuses on the data that is NOT mutated.
-   */
-  test("what goes in, must go out - 'at rest' data", async () => {
-    // prepare action handler mock
-    const onAction = jest.spyOn(console, 'log');
+const { textTest } = testFixtures;
 
-    render(textTest);
+describe('DataText', () => {
+  // without ModuleSandbox will render the property information into a span
+  describe('display', () => {
+    // We need a place to store the view for snapshot testing. This is not required when we are using `screen` directly from RTL.
+    let view;
 
-    // wait for checkbox to be present
-    await waitFor(() => screen.findByRole('checkbox', { name: 'Checkbox' }));
+    beforeEach(() => {
+      // Set the render container to our `view` so it is in scope for the snapshot test
+      view = render(display).container;
+    });
 
-    // get the checkbox and check it
-    const checkboxInput = screen.getByRole('checkbox', { name: 'Checkbox' });
-    fireEvent.change(checkboxInput, { target: { checked: true } });
+    test('snapshot', () => {
+      expect(view).toMatchSnapshot();
+    });
 
-    // submit the form
-    const saveButton = screen.getByRole('button', { name: 'Save' });
-    fireEvent.click(saveButton);
-
-    // wait for the form to be in display mode
-    await waitFor(() => screen.findByRole('button', { name: 'Edit' }));
-
-    /**
-     * We expect the mock to have been called with two params:
-     *
-     * - the action object which includes the action name
-     * - the data sibmitted by the form
-     */
-    expect(onAction).toBeCalledWith(
-      { name: 'edit.save' },
-      {
-        checkbox: true,
-        text: 'Espressive',
-      }
-    );
-
-    /**
-     * We expect that outbound data type is the same as
-     * the inblound data type.
-     */
-    const submittedValue = onAction.mock.calls[0][1].text;
-    const submittedValueType = typeof submittedValue;
-    expect(submittedValueType).toBe('string');
+    test('renders a <span> by default', () => {
+      const input = screen.getByText(displayProps.label);
+      // Make sure the actual DOM element is not render an input
+      expect(input.tagName).toMatch('SPAN');
+      // Make sure the dom element that has our aria-label is the input
+      expect(input.classList.contains('LabelText')).toBe(true);
+    });
   });
 
-  /**
-   * This test validates the data types going in are also going out.
-   * i.e. without any type casting that corrupts the data.
-   *
-   * The test focuses on the data that is mutated.
-   */
-  test("what goes in, must go out - 'mutated' data", async () => {
-    // prepare action handler mock
-    const onAction = jest.spyOn(console, 'log');
+  describe('editing', () => {
+    // We need a place to store the view for snapshot testing. This is not required when we are using `screen` directly from RTL.
+    let view;
 
-    render(textTest);
+    beforeEach(() => {
+      // Set the render container to our `view` so it is in scope for the snapshot test
+      view = render(editing).container;
+    });
 
-    // wait for input to be present
-    await waitFor(() => screen.findByRole('textbox', { name: 'Text' }));
+    test('snapshot', () => {
+      expect(view).toMatchSnapshot();
+    });
 
-    // make the form dirty
-    const textInput = screen.getByRole('textbox', { name: 'Text' });
-    userEvent.type(textInput, ' rocks', { allAtOnce: true });
+    test('renders a <input date> by default', () => {
+      const input = screen.getByLabelText(editingProps.label);
+      // Check that we also use the correct type
+      expect(input).toHaveAttribute('type', 'text');
+      expect(input).toHaveValue('my text value example');
+    });
 
-    // submit the form
-    const saveButton = screen.getByRole('button', { name: 'Save' });
-    fireEvent.click(saveButton);
+    test('change value', () => {
+      const newText = 'new text';
+      const input = screen.getByLabelText(editingProps.label);
+      fireEvent.change(input, { target: { value: newText } });
+      expect(input).toHaveValue(newText);
+    });
+  });
 
-    // wait for the form to be in display mode
-    await waitFor(() => screen.findByRole('button', { name: 'Edit' }));
+  describe('accessibility', () => {
+    test('editing', () => {
+      render(editing);
 
+      const input = screen.getByLabelText(editingProps.label);
+      // The label tag is the parent wrapper
+      const label = input.closest('label');
+
+      // Test is written this way to make sure we know that both values need to be the same.
+      const linkedLabelValue = editingProps.label;
+
+      // Verify label for attribute has linked value
+      expect(label).toHaveAttribute(
+        'for',
+        expect.stringContaining(linkedLabelValue)
+      );
+      // Verify input id attribute has linked value
+      expect(input).toHaveAttribute(
+        'id',
+        expect.stringContaining(linkedLabelValue)
+      );
+      // Check that the input does NOT have an aria-label defined because there is a label tag
+      expect(input).not.toHaveAttribute('aria-label');
+    });
+
+    test('editing no label', () => {
+      // Make sure that the input is still accessible with label text even when we are not showing a label tag in tables
+      render(editingNoLabel);
+      const input = screen.getByLabelText(editingProps.label);
+      expect(input).toBeDefined();
+    });
+  });
+
+  describe('data types', () => {
     /**
-     * We expect the mock to have been called with two params:
+     * This test validates the data types going in are also going out.
+     * i.e. without any type casting that corrupts the data.
      *
-     * - the action object which includes the action name
-     * - the data sibmitted by the form
+     * The test focuses on the data that is NOT mutated.
      */
-    expect(onAction).toBeCalledWith(
-      { name: 'edit.save' },
-      {
-        checkbox: false,
-        text: 'Espressive rocks',
-      }
-    );
+    test("what goes in, must go out - 'at rest' data", async () => {
+      // prepare action handler mock
+      const onAction = jest.spyOn(console, 'log');
+
+      render(textTest);
+
+      // wait for checkbox to be present
+      await waitFor(() => screen.findByRole('checkbox', { name: 'Checkbox' }));
+
+      // get the checkbox and check it
+      const checkboxInput = screen.getByRole('checkbox', { name: 'Checkbox' });
+      fireEvent.change(checkboxInput, { target: { checked: true } });
+
+      // submit the form
+      const saveButton = screen.getByRole('button', { name: 'Save' });
+      fireEvent.click(saveButton);
+
+      // wait for the form to be in display mode
+      await waitFor(() => screen.findByRole('button', { name: 'Edit' }));
+
+      /**
+       * We expect the mock to have been called with two params:
+       *
+       * - the action object which includes the action name
+       * - the data sibmitted by the form
+       */
+      expect(onAction).toBeCalledWith(
+        { name: 'edit.save' },
+        {
+          checkbox: true,
+          text: 'Espressive',
+        }
+      );
+
+      /**
+       * We expect that outbound data type is the same as
+       * the inblound data type.
+       */
+      const submittedValue = onAction.mock.calls[0][1].text;
+      const submittedValueType = typeof submittedValue;
+      expect(submittedValueType).toBe('string');
+    });
 
     /**
-     * We expect that outbound data type is the same as
-     * the inblound data type.
+     * This test validates the data types going in are also going out.
+     * i.e. without any type casting that corrupts the data.
+     *
+     * The test focuses on the data that is mutated.
      */
-    const submittedValue = onAction.mock.calls[0][1].text;
-    const submittedValueType = typeof submittedValue;
-    expect(submittedValueType).toBe('string');
+    test("what goes in, must go out - 'mutated' data", async () => {
+      // prepare action handler mock
+      const onAction = jest.spyOn(console, 'log');
+
+      render(textTest);
+
+      // wait for input to be present
+      await waitFor(() => screen.findByRole('textbox', { name: 'Text' }));
+
+      // make the form dirty
+      const textInput = screen.getByRole('textbox', { name: 'Text' });
+      userEvent.type(textInput, ' rocks', { allAtOnce: true });
+
+      // submit the form
+      const saveButton = screen.getByRole('button', { name: 'Save' });
+      fireEvent.click(saveButton);
+
+      // wait for the form to be in display mode
+      await waitFor(() => screen.findByRole('button', { name: 'Edit' }));
+
+      /**
+       * We expect the mock to have been called with two params:
+       *
+       * - the action object which includes the action name
+       * - the data sibmitted by the form
+       */
+      expect(onAction).toBeCalledWith(
+        { name: 'edit.save' },
+        {
+          checkbox: false,
+          text: 'Espressive rocks',
+        }
+      );
+
+      /**
+       * We expect that outbound data type is the same as
+       * the inblound data type.
+       */
+      const submittedValue = onAction.mock.calls[0][1].text;
+      const submittedValueType = typeof submittedValue;
+      expect(submittedValueType).toBe('string');
+    });
   });
 });
