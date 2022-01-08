c74068a494f862c92c2f43c31bf6d24b08b58a8b
rocio.baena <rocio.baena@espressive.com>
2022-01-08 12:41:38 -0600

    chore(FDS-428):[Cascara ]: Remove data-testid attribute from Cascara components

--- packages/cascara/src/modules/DataDate/DataDate.fixture.js
+++ packages/cascara/src/modules/DataDate/DataDate.fixture.js
@@ -2,42 +2,32 @@ import React from 'react';
 import DataDate from './DataDate';
 import ModuleSandbox from '../ModuleSandbox';
 
-const DisplayDataDate = ({ isEditing, value, ...rest }) => (
+const DisplayDataDate = ({ isEditing, ...rest }) => (
   <ModuleSandbox isEditing={isEditing}>
-    <DataDate
-      {...rest}
-      label='Date'
-      max={'2021-07-25'}
-      min={'2021-07-23'}
-      value={value}
-    />
+    <DataDate {...rest} />
   </ModuleSandbox>
 );
 
-const DataDateModule = () => {
-  return (
-    <>
-      <div>
-        <h1>Date input field</h1>
-        <p>
-          This fixture is for testing the presentation of an input date field
-        </p>
-      </div>
+const value = '2021-07-24';
 
-      <hr />
-
-      <div>
-        <h2>Display</h2>
-        <DisplayDataDate value={'2021-07-24'} />
-      </div>
-
-      <div>
-        <h2>Editing</h2>
-        <DisplayDataDate isEditing value={'2021-07-24'} />
-      </div>
-    </>
-  );
+const displayProps = {
+  label: 'Display',
+  value: value,
 };
 
-export { DisplayDataDate };
-export default <DataDateModule />;
+const editingProps = {
+  isEditing: true,
+  label: 'Editing',
+  value,
+};
+
+// These can be used in tests
+export { displayProps, editingProps };
+
+// These are our fixtures
+export default {
+  display: <DisplayDataDate {...displayProps} />,
+  displayNoLabel: <DisplayDataDate {...displayProps} isLabeled={false} />,
+  editing: <DisplayDataDate {...editingProps} />,
+  editingNoLabel: <DisplayDataDate {...editingProps} isLabeled={false} />,
+};
