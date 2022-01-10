bcb9d437cab05a0542b9fd2ea24014cacb584794
Brian Espinosa <b@bje.co>
2022-01-10 12:15:39 -0800

    untrack node_modules directories

--- node_modules/@fluentui/react-northstar/dist/dts/src/themes/teams-v2/componentVariables.d.ts
+++
@@ -1,2 +0,0 @@
-export { chatVariables as Chat } from './components/Chat/chatVariables';
-export { chatMessageVariables as ChatMessage } from './components/Chat/chatMessageVariables';
--- node_modules/react-cosmos-playground2/dist/cosmos.decorator.d.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-declare const _default: ({ children }: {
-    children: React.ReactNode;
-}) => JSX.Element;
-export default _default;
