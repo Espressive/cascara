8d00ce6890c23399325493732f4707b77d89e48b
lordpixel <darkzoft@gmail.com>
2022-01-13 13:56:41 -0600

    chore(FDS-442): [docs] - vercel building ok

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,5 +1,6 @@
 const withTM = require('next-transpile-modules')([
   '@espressive/cascara',
+  '@espressive/prop-types',
   '@espressive/icons',
 ]);
 const currentGitBranch = require('current-git-branch');
