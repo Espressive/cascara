8d575715d65d803b573d07b39217db426744fb6f
Brian Espinosa <b@bje.co>
2022-01-19 16:19:46 -0800

    Fix comments in test

--- docs/src/components/Rule/Rule.test.js
+++ docs/src/components/Rule/Rule.test.js
@@ -4,7 +4,7 @@ import { render, screen } from '@testing-library/react';
 import Rule from './Rule';
 
 describe('<Rule />', () => {
-  // run snapshot test for all fixtures.
+  // Runs a snapshot test for the minimum required props
   test('empty snapshot', async () => {
     const view = render(
       <Rule body='Required Body' title='Required Title' />
@@ -12,6 +12,7 @@ describe('<Rule />', () => {
     expect(view).toMatchSnapshot();
   });
 
+  // Snapshot test to validate image presentation AND accessibility
   test('renders accessible images', async () => {
     const ruleTitle = 'My Rule';
     const ruleImageSrc = 'https://placedog.net/500/500';
