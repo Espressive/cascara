91734bbc90392410a2e90eb91e661d3d23d71996
rocio.baena <rocio.baena@espressive.com>
2022-01-21 12:18:21 -0600

    Present Flex.Item component as js code

--- packages/cascara/src/atoms/Flex/index.js
+++ packages/cascara/src/atoms/Flex/index.js
@@ -1,2 +1,1 @@
 export { default } from './Flex';
-export { default as FlexItem } from './FlexItem';
--- packages/cascara/src/atoms/index.js
+++ packages/cascara/src/atoms/index.js
@@ -4,4 +4,3 @@
 
 export { default as Button } from './Button';
 export { default as Flex } from './Flex';
-export { default as FlexItem } from './Flex/FlexItem';
