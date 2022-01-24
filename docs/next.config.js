d162cbee88e00170743ace3be151d112722c63ae
rocio.baena <rocio.baena@espressive.com>
2022-01-24 12:39:08 -0600

    update next to 11 version

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,14 +1,14 @@
-const withTM = require('next-transpile-modules')([
-  '@espressive/cascara',
-  '@espressive/icons',
-]);
+// const withTM = require('next-transpile-modules')([
+//   '@espressive/cascara',
+//   '@espressive/icons',
+// ]);
 const currentGitBranch = require('current-git-branch');
 
-module.exports = withTM({
+module.exports = {
   env: {
     GIT_BRANCH: currentGitBranch(),
   },
   poweredByHeader: false,
   // reactStrictMode: true,
   target: 'serverless',
-});
+};
