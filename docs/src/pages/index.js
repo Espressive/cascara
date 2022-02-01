aae2861a769b9ee08be35776083d41bb623d8ace
Brian Espinosa <brianespinosa@users.noreply.github.com>
2022-02-01 13:18:13 -0800

    chore(FDS-447) RFC Button (#372)
    * Update browserslist
    
    * chore(FDS-447) RFC Button added to docs

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -1,15 +1,48 @@
 import hydrate from 'next-mdx-remote/hydrate';
+import pt from 'prop-types';
 import getMDXTree from '../lib/getMDXTree';
 import { postFilePaths, POSTS_PATH } from '../lib/mdxUtils';
 import MDX_COMPONENTS from '../lib/MDX_COMPONENTS';
 import MDX_OPTIONS from '../lib/MDX_OPTIONS';
+import { AdminStructure, Button, Section } from '@espressive/cascara';
 
-export default function Home({ source, frontMatter }) {
+const propTypes = {
+  frontMatter: pt.shape({
+    description: pt.string,
+    title: pt.string,
+  }),
+  // eslint-disable-next-line react/forbid-prop-types -- AST can be any object
+  source: pt.object,
+};
+
+const Home = ({ source, frontMatter }) => {
   const content = hydrate(source, { components: MDX_COMPONENTS });
-  return content;
-}
+  return (
+    <>
+      <AdminStructure.Drawer>
+        <Section title='Feature Requests' titleAs='h4'>
+          <p>
+            Please file a Request For Comments (RFC) on any new components. The
+            Cascara team will evaluate the request. Include a description of
+            both the use case, and the problems the requested component or
+            functionality might solve.
+          </p>
+          <Button
+            as='a'
+            content='File RFC'
+            href='https://espressive.atlassian.net/secure/CreateIssueDetails.jspa?labels=RFC&priority=10001&pid=10042&issuetype=10001&summary=%5BRFC%5D%3A%20&description=Please%20complete%20this%20request%20for%20comment%2Fcomponent%20and%20leave%20%5BRFC%5D%20in%20the%20summary.'
+            rel='noopener noreferrer'
+            size='small'
+            target='_blank'
+          />
+        </Section>
+      </AdminStructure.Drawer>
+      {content}
+    </>
+  );
+};
 
-export async function getStaticProps() {
+const getStaticProps = async () => {
   const fs = require('fs');
   const path = require('path');
   const matter = require('gray-matter');
@@ -49,4 +82,10 @@ export async function getStaticProps() {
     // Keep this here for updates to the MDX files
     revalidate: 1,
   };
-}
+};
+
+Home.propTypes = propTypes;
+
+export { getStaticProps };
+
+export default Home;
