4bb6ef659b1258a301c231dacefe1930633f6e7a
rocio.baena <rocio.baena@espressive.com>
2022-03-24 14:16:33 -0600

    improve props description

--- packages/cascara/src/atoms/DatePicker/DatePicker.js
+++ packages/cascara/src/atoms/DatePicker/DatePicker.js
@@ -4,7 +4,10 @@ import { DatePicker as AntDatePicker } from 'antd';
 import pt from '@espressive/prop-types';
 
 const propTypes = {
+  /** Function that will hold the result-date selected */
   onChange: pt.function,
+  /** String that will define the type of calendar to present date|week|month|quarter|year */
+  picker: pt.string,
 };
 const DatePicker = ({ onChange, ...rest }) => {
   const handleOnChange = useCallback(
