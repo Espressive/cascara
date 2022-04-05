cae42a4861190e8f964dff885ecf2147c5f49bd0
rociobaena <87443054+rociobaena@users.noreply.github.com>
2022-04-05 12:07:53 -0500

    bugfix(FDS-522): [Pickers] Add locales files for date and time pickers (#425)
    * bugfix(FDS-522): [Pickers] Add locales files for date and time pickers
    
    * fix language for timepicker
    
    * Create smart-islands-decide.md
    
    Co-authored-by: Manuel Ramirez <lordpixel@users.noreply.github.com>

--- packages/cascara/src/atoms/DatePicker/DatePicker.js
+++ packages/cascara/src/atoms/DatePicker/DatePicker.js
@@ -1,24 +1,34 @@
 import React, { useCallback } from 'react';
-import 'antd/dist/antd.css';
+import '../../shared/AntDPicker.css';
 import { DatePicker as AntDatePicker } from 'antd';
 import pt from '@espressive/prop-types';
 import PICKER_TYPE from './_globals';
+import locales from '../../shared/locales';
 
 const propTypes = {
+  /** localize picker */
+  language: pt.string,
   /** Function that will hold the result-date selected */
   onChange: pt.func,
   /** String that will define the type of calendar to present date|week|month|quarter|year */
   picker: pt.oneOf([...Object.keys(PICKER_TYPE), null]),
 };
 
-const DatePicker = ({ onChange, ...rest }) => {
+const DatePicker = ({ onChange, language, ...rest }) => {
   const handleOnChange = useCallback(
     (date, dateString) => {
       onChange(date, dateString);
     },
     [onChange]
   );
-  return <AntDatePicker {...rest} onChange={handleOnChange} />;
+
+  return (
+    <AntDatePicker
+      {...rest}
+      locale={locales[language]}
+      onChange={handleOnChange}
+    />
+  );
 };
 
 DatePicker.propTypes = propTypes;
