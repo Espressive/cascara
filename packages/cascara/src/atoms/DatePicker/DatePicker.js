41a5c0a25409f1379728d4a12d3adca115776be0
rocio.baena <rocio.baena@espressive.com>
2023-04-13 22:47:11 -0600

    chore(DEV-22398) Return format the format selected in the datepicker

--- packages/cascara/src/atoms/DatePicker/DatePicker.js
+++ packages/cascara/src/atoms/DatePicker/DatePicker.js
@@ -3,7 +3,6 @@ import { DatePicker as AntDatePicker } from 'antd';
 import pt from '@espressive/prop-types';
 import PICKER_TYPE from './_globals';
 import locales from '../../shared/locales';
-
 const propTypes = {
   /** date format */
   format: pt.string,
@@ -14,29 +13,27 @@ const propTypes = {
   /** String that will define the type of calendar to present date|week|month|quarter|year */
   picker: pt.oneOf([...Object.keys(PICKER_TYPE), null]),
 };
-
 const DatePicker = ({ format, onChange, lang, ...rest }) => {
+  const defaultLang = 'en-US';
+  const defaultFormat = 'YYYY-MM-DD';
+  const dateFormat = lang ? locales[lang]?.lang?.dateFormat : defaultFormat;
+  const locale = lang ? locales[lang] : locales[defaultLang];
+
   const handleOnChange = useCallback(
     (date, dateString) => {
-      onChange(date, dateString);
+      onChange(date, dateString, dateFormat);
     },
-    [onChange]
+    [dateFormat, onChange]
   );
-
-  const formatForLocales = locales[lang]?.lang?.dateFormat;
-  const validatedFormats =
-    formatForLocales === 'M/D/YYYY' ? 'MM/DD/YYYY' : formatForLocales;
-
   return (
     <AntDatePicker
       {...rest}
       aria-label='Date'
-      format={format || validatedFormats}
-      locale={lang ? locales[lang] : ''}
+      format={format ? format : dateFormat}
+      locale={locale}
       onChange={handleOnChange}
     />
   );
 };
-
 DatePicker.propTypes = propTypes;
 export default DatePicker;
